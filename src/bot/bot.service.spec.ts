3910af6822314b4ab4d1cd7fac5b053aa8644cc4
codecraft26 <gaman0221@gmail.com>
2024-07-20 18:52:19 +0530

    feat: Add Telegram bot integration and handle 'hello' and 'hi' commands
    This commit adds the necessary code changes to integrate a Telegram bot into the application. It includes the creation of the `BotService` class, which implements the `OnModuleInit` interface to handle incoming messages. The bot responds with a random funny statement when receiving the 'hello' or 'hi' commands. If the command is not recognized, it sends a default message.

--- src/bot/bot.service.spec.ts
+++ src/bot/bot.service.spec.ts
@@ -1,18 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { BotService } from './bot.service';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as TelegramBot from 'node-telegram-bot-api';
 
-describe('BotService', () => {
-  let service: BotService;
+@Injectable()
+export class BotService implements OnModuleInit {
+    private bot: TelegramBot;
+    private readonly funnyStatements: string[] = [
+        "Hello there! What, no Hi for me? Why so formal?",
+        "Hi! Did you just 'hello' me? How old-school!",
+        "Hey! What's up? Just 'hello'?",
+        "Hello! Are you a bot too?",
+        "Hi there! Did you just say hello?"
+    ];
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BotService],
-    }).compile();
+    constructor(private configService: ConfigService) {}
 
-    service = module.get<BotService>(BotService);
-  });
+    onModuleInit() {
+        const token = this.configService.get<string>('TELEGRAM_BOT_TOKEN');
+        this.bot = new TelegramBot(token, { polling: true });
+        this.bot.on('message', this.handleMessage.bind(this));
+    }
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    private handleMessage(msg: TelegramBot.Message) {
+        const chatId = msg.chat.id;
+        const text = msg.text.toLowerCase();
+
+        if (text === 'hello'|| text === 'hi' ) {
+            const randomIndex = Math.floor(Math.random() * this.funnyStatements.length);
+            this.bot.sendMessage(chatId, this.funnyStatements[randomIndex]);
+        } else {
+            this.bot.sendMessage(chatId, "Sorry, I didn't understand that command.");
+        }
+    }
+}
